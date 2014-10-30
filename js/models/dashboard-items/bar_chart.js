27d7d03c3826d5023891e13d647850999755c9c4
jearoyer <jroyer@graphite.(none)>
2014-10-30 10:08:31 +0100

    Add of bar chart and discrete bar chart

---
+++ js/models/dashboard-items/bar_chart.js
@@ -0,0 +1,34 @@
+ds.register_dashboard_item('bar_chart', {
+
+  display_name: 'Bar Chart X',
+  icon: 'fa fa-image',
+  category: 'chart',
+
+  constructor: function(data) {
+    'use strict'
+
+    var self = limivorous.observable()
+                         .extend(ds.models.item, {item_type: 'bar_chart'})
+                         .extend(ds.models.chart)
+                         .build()
+
+    ds.models.chart.init(self, data)
+    ds.models.item.init(self, data)
+
+    self.toJSON = function() {
+      return ds.models.chart.json(self, ds.models.item.json(self))
+    }
+
+    return self
+  },
+
+  data_handler: function(query, item) {
+    ds.charts.bar_chart($('#' + item.item_id + ' .ds-graph-holder'), item, query)
+  },
+
+  template: ds.templates.models.bar_chart,
+
+  interactive_properties: ds.models.chart.interactive_properties
+                            .concat(ds.models.item.interactive_properties)
+
+})
