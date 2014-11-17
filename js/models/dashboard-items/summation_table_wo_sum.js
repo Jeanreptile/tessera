148af2bb50234fd9d7385e63ca835149d5cec66b
kalw <regis.despres@gmail.com>
2014-11-17 14:37:40 +0000

    minor bugfixes ; new template added - config sample added and anoymized

---
+++ js/models/dashboard-items/summation_table_wo_sum.js
@@ -0,0 +1,54 @@
+ds.register_dashboard_item('summation_table_wo_sum', {
+
+  display_name: 'Summation Table wo/sum',
+  icon: 'fa fa-table',
+  category: 'data-table',
+
+  constructor: function(data) {
+    'use strict'
+
+    var self = limivorous.observable()
+                         .property('striped', {init: false})
+                         .property('format', {init: ',.3s'})
+                         .property('title')
+                         .extend(ds.models.item, {item_type: 'summation_table_wo_sum'})
+                         .build()
+    Object.defineProperty(self, 'requires_data', {value: true})
+
+    if (data) {
+      self.striped = data.striped !== false
+      self.title = data.title
+      self.format = data.format || self.format
+    }
+    ds.models.item.init(self, data)
+
+    self.toJSON = function() {
+      var data = ds.models.item.json(self)
+      if (self.format)
+        data.format = self.format
+      if (self.striped)
+        data.striped = self.striped
+      if (self.title)
+        data.title = self.title
+      return data
+    }
+
+    return self
+  },
+
+  data_handler: function(query, item) {
+    var body = $('#' + item.item_id + ' tbody')
+    body.empty()
+    query.data.forEach(function(series) {
+      body.append(ds.templates.models.summation_table_row_wo_sum({series:series, item:item}))
+    })
+  },
+
+  template: ds.templates.models.summation_table_wo_sum,
+
+  interactive_properties: [
+    { id: 'striped', type: 'boolean' },
+    'format',
+    'title',
+  ].concat(ds.models.item.interactive_properties)
+})
